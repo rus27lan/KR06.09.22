d77daadba8f8ef782446f4e90ef8384a13802874
Rus27lan <ruslan27.11.96@mail.ru>
2022-09-06 20:03:28 +0500

    TASK COMPLETED

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿Console.WriteLine("Введите длинну массива: ");
+int array = int.Parse(Console.ReadLine());
+void ArrayPrintLen (int array)
+{
+    Console.WriteLine("Что нужно записать в массив: ");
+    string [] arr = new string[array];
+    int num = 0;
+    while(num < arr.Length)
+    {
+        string text = Console.ReadLine();
+        string count_num = text;
+        arr[num] = count_num;
+        num++;  
+    }
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3) Console.Write($"{arr[i]} ");
+    }
+}
+ArrayPrintLen(array);
